0b527e735965c0edcc49758cacb1aec4e808f4a4
ToruShimizu <nobbyz.t.nn@gmail.com>
2024-11-24 18:30:22 +0900

    chore: tailwindcssを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import cloudflare from '@astrojs/cloudflare';
 
 import sitemap from '@astrojs/sitemap';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://kikankou-tech.com",
   output: 'server',
   adapter: cloudflare(),
-  integrations: [sitemap()],
+  integrations: [sitemap(), tailwind()],
 });
