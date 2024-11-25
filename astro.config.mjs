4060b49e1aebe30d5b8c5b91607c750e841d5dc5
ToruShimizu <nobbyz.t.nn@gmail.com>
2024-11-25 23:59:43 +0900

    chore: configを修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,14 @@ import sitemap from '@astrojs/sitemap'
 
 import tailwind from '@astrojs/tailwind'
 
-import react from '@astrojs/react';
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://kikankou-tech.com',
     output: 'server',
-    adapter: cloudflare(),
+    adapter: cloudflare({
+        imageService: 'cloudflare',
+    }),
     integrations: [sitemap(), tailwind(), react()],
 })
