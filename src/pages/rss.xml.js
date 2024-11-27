4b18a1f7c9fd660a1bc35ddb1d094ae95b66a3eb
ToruShimizu <nobbyz.t.nn@gmail.com>
2024-11-27 20:46:23 +0900

    fix: rssを修正

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,25 @@
-import rss from '@astrojs/rss'
+import { getRssString } from '@astrojs/rss'
+import { getBlogs } from '../library/microcms'
+import { META_TITLE, META_DESCRIPTION } from './constants/meta'
 
-export async function GET(context) {
-    return rss({
-        title: 'シミティーブログ',
-        description:
-            '30歳で工場勤務からWebエンジニアに転職した男性の、キャリアチェンジへの決意と葛藤。安定した仕事を捨てる覚悟、将来への不安、そして挑戦する勇気を赤裸々に語る転職体験記。未経験からのキャリアチェンジに悩む全ての人へのメッセージ。',
+export const GET = async (context) => {
+    const posts = await getBlogs()
+
+    const rss = await getRssString({
+        title: META_TITLE,
+        description: META_DESCRIPTION,
         site: context.site,
-        items: [],
+        items: posts.contents.map((post) => ({
+            link: new URL(`blog/${post.id}`, context.site.href).href,
+            title: post.title,
+            description: `${post.description}...`,
+            pubDate: post.publishedAt,
+            content: post.content,
+        })),
+    })
+    return new Response(rss, {
+        headers: {
+            'Content-Type': 'application/xml',
+        },
     })
 }
