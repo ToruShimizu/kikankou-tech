ec9e44092b09b6f4b60d3b9b1473a15d53e69574
ToruShimizu <nobbyz.t.nn@gmail.com>
2024-11-27 23:26:47 +0900

    fix: 日付を修正

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,8 @@
 import { getRssString } from '@astrojs/rss'
 import { getBlogs } from '../library/microcms'
 import { META_TITLE, META_DESCRIPTION } from './constants/meta'
+import { ja } from 'date-fns/locale'
+import { format } from 'date-fns'
 
 export const GET = async (context) => {
     const posts = await getBlogs({
@@ -12,17 +14,19 @@ export const GET = async (context) => {
         title: META_TITLE,
         description: META_DESCRIPTION,
         site: context.site,
-        items: posts.contents.map((post) => ({
-            link: new URL(`blog/${post.id}`, context.site.href).href,
-            title: post.title,
-            description: `${post.description}...`,
-            pubDate: new Date(post.publishedAt).toLocaleDateString('ja-JP', {
-                year: 'numeric',
-                month: 'short',
-                day: 'numeric',
-            }),
-            content: post.content,
-        })),
+        items: posts.contents.map((post) => {
+            const date = format(new Date(post.publishedAt), 'yyyy-MM-dd', {
+                locale: ja,
+            })
+
+            return {
+                link: new URL(`blog/${post.id}`, context.site.href).href,
+                title: post.title,
+                description: `${post.description}...`,
+                pubDate: date,
+                content: post.content,
+            }
+        }),
     })
     return new Response(rss, {
         headers: {
