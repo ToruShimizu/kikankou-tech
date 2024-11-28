5d88b2d2b712a843943aa015710a4ca0f1cc0fa7
ToruShimizu <nobbyz.t.nn@gmail.com>
2024-11-28 23:11:49 +0900

    chore: playformCompressを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 
 import react from '@astrojs/react'
+import playformCompress from '@playform/compress'
 
 // https://astro.build/config
 export default defineConfig({
@@ -21,5 +22,10 @@ export default defineConfig({
         }),
         tailwind(),
         react(),
+        playformCompress({
+            CSS: false,
+            HTML: false,
+            JavaScript: false,
+        }),
     ],
 })
